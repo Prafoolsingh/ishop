8ce01064055a319419f09a895a1e7960b3ed5c25
Prafool singh <139557280+Prafoolsingh@users.noreply.github.com>
2024-03-24 01:25:01 +0530

    some changes

--- frontend/src/components/layout/header/AdminHeader.jsx
+++ frontend/src/components/layout/header/AdminHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LogoutIcon from '@mui/icons-material/Logout';
 import "./Header.css";
 import { toast } from "react-toastify"
@@ -7,7 +7,6 @@ import { logout } from '../../../actions/user_action';
 import { useDispatch } from "react-redux";
 
 const AdminHeader = () => {
-  const [isTogglerOpen, setIsTogglerOpen] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -17,36 +16,32 @@ const AdminHeader = () => {
     navigate('/login');
   };
 
-  const handleLinkClick = () => {
-    setIsTogglerOpen(false); // Close the toggler menu when a link is clicked
-  };
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Ishop</Link>
-        <button className="navbar-toggler" type="button" onClick={() => setIsTogglerOpen(!isTogglerOpen)} aria-expanded={isTogglerOpen ? "true" : "false"}>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNavbar" aria-controls="adminNavbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className={`collapse navbar-collapse ${isTogglerOpen ? 'show' : ''}`} id="navbarNav">
+        <div className="collapse navbar-collapse" id="adminNavbar">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-3 mx-md-0 mx-sm-0 mx-0" to="/admin/dashboard">Dashboard</Link>
+              <Link className="nav-link mx-lg-3 mx-md-0 mx-sm-0 mx-0" to="/admin/dashboard">Dashboard</Link>
             </li>
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-3 mx-md-0 mx-sm-0 mx-0" to="/admin/products">Products</Link>
+              <Link className="nav-link mx-lg-3 mx-md-0 mx-sm-0 mx-0" to="/admin/products">Products</Link>
             </li>
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/product/create">Create Products</Link>
+              <Link className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/product/create">Create Products</Link>
             </li>
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/orders">Orders</Link>
+              <Link className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/orders">Orders</Link>
             </li>
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/users">Users</Link>
+              <Link className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/users">Users</Link>
             </li>
             <li className="nav-item">
-              <Link onClick={handleLinkClick} className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/reviews">Reviews</Link>
+              <Link className="nav-link mx-lg-4 mx-md-0 mx-sm-0 mx-0" to="/admin/reviews">Reviews</Link>
             </li>
             <li className="nav-item">
               <button onClick={handleLogout} className="nav-link btn mx-lg-4 mx-md-0 mx-sm-0 mx-0">Logout<LogoutIcon/></button>
